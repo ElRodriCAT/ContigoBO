1ddfb123e3e9fcb2f591a2ca519815c1f9607d74
Rodrigo Moyano <rodrigomoyanocat@gmail.com>
2025-09-18 15:00:21 -0300

    Revert "agregando loggin y validaciones"
    This reverts commit 2a84ceb4e52b9f148dd25b245a42811e0adc94f7.

--- src/routes/usuario.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
-
-
-router.post('/', usuarioController.crear);
-router.post('/login', usuarioController.login);
-
-module.exports = router;
